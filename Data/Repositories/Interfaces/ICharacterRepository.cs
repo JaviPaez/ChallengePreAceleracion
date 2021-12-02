8a1e42373d09d2bb289245384936d0dace3cb69f
JaviPaez <69802155+JaviPaez@users.noreply.github.com>
2021-12-02 08:08:48 -0300

    Update ICharacterRepository.cs

--- Data/Repositories/Interfaces/ICharacterRepository.cs
+++ Data/Repositories/Interfaces/ICharacterRepository.cs
@@ -6,10 +6,10 @@ namespace Data.Repositories
     public interface ICharacterRepository
     {
         IEnumerable<Character> GetAll();
-        Character Get(int CharacterId);
+        Character Get(int characterId);
         void Insert(Character character);
         void Update(Character character);
-        void Delete(int CharacterId);
+        void Delete(int characterId);
         void Save();
     }
 }
