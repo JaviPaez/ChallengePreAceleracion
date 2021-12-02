700526a95d18326bca65cc7dfdb728bd1939f4b4
JaviPaez <69802155+JaviPaez@users.noreply.github.com>
2021-12-02 08:09:29 -0300

    Update IMovieRepository.cs

--- Data/Repositories/Interfaces/IMovieRepository.cs
+++ Data/Repositories/Interfaces/IMovieRepository.cs
@@ -6,10 +6,10 @@ namespace Data.Repositories
     public interface IMovieRepository
     {
         IEnumerable<Movie> GetAll();
-        Movie Get(int MovieId);
+        Movie Get(int movieId);
         void Insert(Movie movie);
         void Update(Movie movie);
-        void Delete(int MovieId);
+        void Delete(int movieId);
         void Save();
     }
 }
