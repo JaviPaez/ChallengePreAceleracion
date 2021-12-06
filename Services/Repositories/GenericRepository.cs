056eda0eebb74b300e08c1aebddbf1a3a5e98cc2
Javier Paez <javierignacio.paez@gmail.com>
2021-12-05 23:16:50 -0300

    GenericRepository

--- Services/Repositories/GenericRepository.cs
+++ Services/Repositories/GenericRepository.cs
@@ -1,8 +1,6 @@
 using Domain;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Services.Repositories
@@ -16,31 +14,56 @@ namespace Services.Repositories
             _dbContext = dbContext;
         }
 
-        public List<T> GetAll()
-        {
-            return _dbContext.Set<T>().ToList();
-        }
-
-        public T Get(int id)
-        {
-            return _dbContext.Set<T>().Find(id);
-        }
-
         public async Task<T> AddAsync(T entity)
         {
-            _dbContext.Set<T>().Add(entity);
+            await _dbContext.Set<T>().AddAsync(entity);
             _dbContext.SaveChanges();
+
             return entity;
         }
 
-        public T Update(int id) //(TEntity entity)
+        public async Task<int> CountAsync()
         {
-            throw new NotImplementedException();
+            var quantity = await _dbContext.Set<T>().CountAsync();
+
+            return quantity;
         }
 
-        public T Delete(int id)
+        public bool EntityExists(int id)
         {
-            throw new NotImplementedException();
+            bool exists;
+            if (_dbContext.Set<T>().Find(id) != null) exists = true;
+            else exists = false;
+
+            return exists;
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            var entity = await _dbContext.Set<T>().FindAsync(id);
+
+            return entity;
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _dbContext.Set<T>().Update(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> DeleteAsync(int id)
+        {
+            var entity =  _dbContext.Set<T>().Find(id);
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
         }
     }
 }
